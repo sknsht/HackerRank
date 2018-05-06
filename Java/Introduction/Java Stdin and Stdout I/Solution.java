60153bde3778302e89e727040f8b17fc8af38bed
sofia <sofiakinsht@gmail.com>
2018-05-06 14:47:52 +0200

    Solutions to Java Introduction section

---
+++ Java/Introduction/Java Stdin and Stdout I/Solution.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextInt()) {
+            System.out.println(sc.nextInt());
+        }
+    }
+}
