f605405515cf6ce1ff4da58630964d4f2a659682
sofia <sofiakinsht@gmail.com>
2018-05-07 22:07:39 +0200

    Solutions to Java Introduction section

---
+++ Java/Introduction/Java If-Else/Solution.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();            
+        String ans = "";
+        if(n % 2 == 1 || (n >= 6 && n <= 20)) {ans = "Weird";}
+        else {ans = "Not Weird";}
+        System.out.println(ans);  
+    }
+}
