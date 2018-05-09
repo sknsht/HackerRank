ba6abade29d14cde86ebc7bd19c9f51650abec6d
sofia <sofiakinsht@gmail.com>
2018-05-09 21:38:19 +0200

    Solutions to Java Introduction section

---
+++ Java/Introduction/Java Output Formatting/Solution.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("================================");
+            for(int i = 0; i < 3; i++) {
+                String s = sc.next();
+                int x = sc.nextInt();
+                System.out.printf("%-15s%03d%n", s, x);
+            }
+            System.out.println("================================");
+    }
+}
