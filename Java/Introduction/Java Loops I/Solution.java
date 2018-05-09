c81a18e96fef66bb2f2b27ef064e293779dc0699
sofia <sofiakinsht@gmail.com>
2018-05-09 21:43:01 +0200

    Solutions to Java Introduction section

---
+++ Java/Introduction/Java Loops I/Solution.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " x " + i + " = " + i * n);
+        }
+    }
+}
