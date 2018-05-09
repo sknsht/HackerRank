38599bc910155c174cfc43dd5378288ed18403d2
sofia <sofiakinsht@gmail.com>
2018-05-09 21:52:23 +0200

    Solutions to Java Introduction section

---
+++ Java/Introduction/Java End-of-file/Solution.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = 0;
+        while (sc.hasNext()) {
+            System.out.println(++n + " " + sc.nextLine());
+        }
+    }
+}
