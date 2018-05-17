934ea2c7c5073b9e08188656489718476faaaa2e
sofia <sofiakinsht@gmail.com>
2018-05-17 21:18:18 +0200

    Solutions to Java Data Object Oriented Programming section

---
+++ Java/Object Oriented Programming/Java Inheritance II/Solution.java
@@ -0,0 +1,23 @@
+class Arithmetic {
+    int add(int x, int y) {
+        return x + y;
+    }
+}
+
+class Adder extends Arithmetic {
+    Adder() {
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) {
+        // Create a new Adder object
+        Adder a = new Adder();
+
+        // Print the name of the superclass on a new line
+        System.out.println("My superclass is: " + a.getClass().getSuperclass().getName());
+
+        // Print the result of 3 calls to Adder's `add(int,int)` method as 3 space-separated integers:
+        System.out.print(a.add(10, 32) + " " + a.add(10, 3) + " " + a.add(10, 10) + "\n");
+    }
+}
