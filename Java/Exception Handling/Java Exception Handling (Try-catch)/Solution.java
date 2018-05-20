726bb093a45edcd3b0c68ff334c204052b9f22c1
sofia <sofiakinsht@gmail.com>
2018-05-20 20:12:22 +0200

    Solutions to Java Exception Handling section

---
+++ Java/Exception Handling/Java Exception Handling (Try-catch)/Solution.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        try {
+            int x = scan.nextInt();
+            int y = scan.nextInt();
+            System.out.println(x / y);
+        } catch (InputMismatchException e) {
+            System.out.println(e.getClass().getName());
+        } catch (ArithmeticException e) {
+            System.out.println(e.getClass().getName() + ": / by zero");
+        }
+    }
+}
---
+++ Java/Exception Handling/Java Exception Handling/Solution.java
@@ -0,0 +1,10 @@
+class MyCalculator {
+    long power(int n, int p) throws Exception {
+        if (n < 0 || p < 0) {
+            throw new Exception("n or p should not be negative.");
+        } else if (n == 0 && p == 0) {
+            throw new Exception("n and p should not be zero.");
+        }
+        return (long) Math.pow(n, p);
+    }
+}
