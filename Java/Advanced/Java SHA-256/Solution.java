6a42083431507397471cf615e60bfbad2508cb2b
sofia <sofiakinsht@gmail.com>
2018-05-21 22:33:51 +0200

    Solutions to Java Advanced section

---
+++ Java/Advanced/Java SHA-256/Solution.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Solution {
+
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        Scanner scan = new Scanner(System.in);
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        md.update(scan.next().getBytes());
+        scan.close();
+
+        byte[] digest = md.digest();
+
+        for (byte b : digest) {
+            System.out.format("%02x", b);
+        }
+    }
+}
