edc29628d41707bf09b7a09436f674c12c97405f
sofia <sofiakinsht@gmail.com>
2018-05-21 22:28:13 +0200

    Solutions to Java Advanced section

---
+++ Java/Advanced/Java Reflection - Attributes/Solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+
+    public static void main(String[] args) {
+        Class student = Student.class;
+        Method[] methods = student.getDeclaredMethods();
+
+        ArrayList<String> methodList = new ArrayList<>();
+        for (Method method : methods) {
+            methodList.add(method.getName());
+        }
+        Collections.sort(methodList);
+        for (String name : methodList) {
+            System.out.println(name);
+        }
+    }
+}
