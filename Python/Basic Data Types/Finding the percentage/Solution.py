c4dbd18989fb82bf007433a85f56623244ffef72
sofia <sofiakinsht@gmail.com>
2018-05-29 08:14:52 +0200

    Solutions to Python Basic Data Types section

---
+++ Python/Basic Data Types/Finding the percentage/Solution.py
@@ -0,0 +1,11 @@
+n = int(input())
+student_marks = {}
+for _ in range(n):
+    name, *line = input().split()
+    scores = list(map(float, line))
+    student_marks[name] = scores
+query_name = input()
+
+query_scores = student_marks[query_name]
+avg = sum(query_scores) / 3.0
+print("%.2f" % avg)
