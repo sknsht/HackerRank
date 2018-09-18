e68d6eb14a7b81eadccd1dc68cca319378e5892e
sofia <sofiakinsht@gmail.com>
2018-09-18 09:56:29 +0200

    Solutions to Arrays section

---
+++ Interview Preparation Kit/Arrays/Minimum Swaps 2/Solution.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    
+    static int minimumSwaps(int[] array) {
+        int n = array.length - 1;
+        int minSwaps = 0;
+        for (int i = 0; i < n; i++) {
+            if (i < array[i] - 1) {
+                swap(array, i, Math.min(n, array[i] - 1));
+                minSwaps++;
+                i--;
+            }
+        }
+        return minSwaps;
+    }
+
+    private static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int res = minimumSwaps(arr);
+
+        bufferedWriter.write(String.valueOf(res));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
