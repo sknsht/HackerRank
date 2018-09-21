7771f44fade8e0c5fb3b99d7ae98b2037b4bc125
sofia <sofiakinsht@gmail.com>
2018-09-21 10:43:38 +0200

    Solutions to Dictionaries and Hashmaps section

---
+++ Interview Preparation Kit/Dictionaries and Hashmaps/Two Strings/Solution.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static String twoStrings(String s1, String s2) {
+        if (Pattern.compile("[" + s2 + "]").matcher(s1).find())
+            return "YES";
+        return "NO";
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s1 = scanner.nextLine();
+
+            String s2 = scanner.nextLine();
+
+            String result = twoStrings(s1, s2);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
