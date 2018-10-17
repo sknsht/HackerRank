e9f4d315dff9eff8d44776d3cb09195dcc2eee1d
sofia <sofiakinsht@gmail.com>
2018-10-17 07:51:14 +0200

    Solutions to Search section

---
+++ Interview Preparation Kit/Search/Pairs/Solution.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    // Complete the pairs function below.
+    static int pairs(int k, int[] arr) {
+        Arrays.sort(arr);
+
+        int n = arr.length;
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (Arrays.binarySearch(arr, arr[i] - k) > -1) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nk = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int result = pairs(k, arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
