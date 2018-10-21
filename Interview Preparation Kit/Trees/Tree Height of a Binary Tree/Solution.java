444e936ab95202f00fee1357be12de11084ccc82
sofia <sofiakinsht@gmail.com>
2018-10-21 14:47:55 +0200

    Solutions to Trees section

---
+++ Interview Preparation Kit/Trees/Tree Height of a Binary Tree/Solution.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Solution {
+    public static int getHeight(Node root) {
+        if (root == null)
+            return -1;
+        return 1 + Math.max(getHeight(root.left), getHeight(root.right));
+    }
+
+    public static Node insert(Node root, int data) {
+        if (root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if (data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while (t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        scan.close();
+        int height = getHeight(root);
+        System.out.println(height);
+    }
+}
