2fa7b33692b9615fbcf6d67e97913449a986e6fd
sofia <sofiakinsht@gmail.com>
2018-10-28 20:14:45 +0100

    Solutions to Miscellaneous section

---
+++ Interview Preparation Kit/Miscellaneous/Flipping bits/Solution.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    // Complete the flippingBits function below.
+    static long flippingBits(long n) {
+        long maxValue = (long) Math.pow(2, 32) - 1;
+        return n ^ maxValue;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            long n = scanner.nextLong();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            long result = flippingBits(n);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
