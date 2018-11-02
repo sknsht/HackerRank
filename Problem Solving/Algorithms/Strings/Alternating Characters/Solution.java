b70ef24436bf4ef2dab4ca6baee8c0ae7c79b4ce
sofia <sofiakinsht@gmail.com>
2018-11-02 10:16:20 +0100

    Solutions to Algorithms section

---
+++ Problem Solving/Algorithms/Strings/Alternating Characters/Solution.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    static int alternatingCharacters(String s) {
+        int numDeletions = 0;
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                numDeletions++;
+            }
+        }
+        return numDeletions;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s = scanner.nextLine();
+
+            int result = alternatingCharacters(s);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
