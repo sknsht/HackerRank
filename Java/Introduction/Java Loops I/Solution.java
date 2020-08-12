4c846ee9593eaa6c473367e5bca7475a3535a91c
sknsht <sofiakinsht@gmail.com>
2020-08-12 20:25:31 +0200

    Update Solution

--- Java/Introduction/Java Loops I/Solution.java
+++ Java/Introduction/Java Loops I/Solution.java
@@ -1,12 +1,14 @@
-import java.util.*;
+import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class Solution {
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(n + " x " + i + " = " + i * n);
-        }
+        int n = scanner.nextInt();
+        IntStream.rangeClosed(1, 10)
+                .mapToObj(i -> String.format("%d x %d = %d", n, i, n * i))
+                .forEach(System.out::println);
+        scanner.close();
     }
 }
